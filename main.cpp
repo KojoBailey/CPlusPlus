07146cbd04ddb4db88daa6d66fe0a10dc72fd35c
Kojo Bailey <50509420+KojoBailey@users.noreply.github.com>
2026-03-01 03:00:46 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+%:import "iostream"
+
+auto main(register int(c), const char*(v)<::>) noexcept -> int
+{
+  return operator<<(std::wclog, *(c ^= 074144, NULL<:v:> + 9))
+    << (typeof(*v))new volatile const signed long long<%c++%>, not true;
+}
